5271531fc719ef44c1cb873c8c12a94e0cf636d8
Ubuntu <delaney.18@wright.edu>
2022-01-26 14:38:38 +0000

    updating file

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -29,25 +29,25 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
+    spongebob_quotes = [
+        'I\'m the one who lives at the bottom of the sea',
+        'SpongeBot!',
         (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
+            'Hey Patrick, what\'s funnier than 24? '
+            '25!'
         ),
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    office_quotes = [
+        'I\'m an early bird and I\'m a night owl. So I\'m wise and I have worms.',
+        'I saved a life. My own. Am I a hero?... I really can\'t say, but yes!',
+        'Nothing stresses me out. Except having to seek the approval of my inferiors.',
+        'Before I do anything I ask myself "Would an idiot do that?" and if the answer is yes I do not do that thing.',
     ]
 
-    if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
-        response = random.choice(hitchhiker_quotes)
+    if message.content == 'apple!':
+       # response = random.choice(spongebob_quotes)
+        response = random.choice(office_quotes)
         await message.channel.send(response)
 
 client.run(TOKEN)
